697f238b83d92f9614621919bb0743a13926d2b4
ananya868 <ananya8154@gmail.com>
2024-11-04 19:18:29 +0530

    src file updated

--- src/data_extraction.py
+++ src/data_extraction.py
@@ -15,7 +15,101 @@ class DataExtractionBase(ABC):
 
 
 
+# Class for extracting text from PDF files
+class PDFTextExtractorForBreeds(DataExtractionBase):
+    def __init__(self, pdf_file_path):
+        self.pdf_file_path = pdf_file_path
 
+    # Extra method to merge short elements in a list of text elements
+    def merge_short_elements(self, item_list: list, text: list):
+        """
+        Merge short elements in a list of text elements
+        About: Some elements in the list of texts (extracted from pdf) might be short, identify those and pass
+                them to this function to merge them with their consecutive elements. 
+        """
+        for t in text:
+            for i in range(len(item_list) - 1):
+                if t in item_list[i]:
+                    # Copy this 
+                    content = item_list[i]
+                    item_list.pop(i)
+                    # Add the new content to next item 
+                    item_list[i] = content + " " + item_list[i]
+                    break 
 
+        return item_list
 
+    # Extract method for extracting text from PDF files 
+    def extract(self):
+        """
+        Extract text from a PDF file
+        """
+        # Open the PDF file using PyMuPDF
+        pdf_reader = fitz.open(self.pdf_file_path)
 
+        # list for storing text
+        text_list = []
+        subheadings = []
+
+        # Iterate through each page in the PDF file
+        for i in range(pdf_reader.page_count):
+            page = pdf_reader[i]
+            blocks = page.get_text("dict")["blocks"] 
+
+            for block in blocks:
+                if "lines" in block:
+                    for line in block["lines"]:
+                        for span in line["spans"]:
+                            text = span["text"].strip()
+                            font_size = span["size"]
+                            font_flags = span["flags"]
+                            
+                            # Condition
+                            if font_size > 12:
+                                text_list.append(text)
+                                subheadings.append(text)
+                            else:
+                                # If not a new section, add it to the current section
+                                if text_list and not text_list[-1].endswith("\n"):
+                                    text_list[-1] += " " + text
+                                else:
+                                    text_list.append(text) # Starts new section
+        
+        text_list_01 = text_list.copy()
+        # Iterate through the list and merge items containing "FAQ" with the next item
+        i = 0
+        while i < len(text_list_01) - 1: 
+            if "FAQ" in text_list_01[i] and "References" not in text_list_01[i]:
+                # Merge current item with the next item unless the next item contains "References"
+                if "References" not in text_list_01[i + 1]:
+                    text_list_01[i] = text_list_01[i] + " " + text_list_01[i + 1]
+                    # Remove the next item (since it's now merged)
+                    text_list_01.pop(i + 1)
+                else:
+                    i += 1  # Move to the next item if the next one contains "References"
+            else:
+                i += 1  # Only move to the next item if no merge happened or if "References" was encountered
+        
+        return text_list_01, subheadings 
+
+    
+
+# Factory class for Data Extraction 
+class DataExtractionFactory:
+    def __init__(self, strategy: DataExtractionBase):
+        """ 
+        Initialize the factory with a strategy
+        """
+        self.strategy = strategy
+
+    def set_strategy(self, strategy: DataExtractionBase):
+        """
+        Set the strategy for data extraction
+        """
+        self.strategy = strategy
+    
+    def extract(self):
+        """
+        Extract data using the set strategy
+        """
+        return self.strategy.extract()
--- src/data_ingestion.py
+++ src/data_ingestion.py
@@ -1,1 +1,89 @@
 from abc import ABC, abstractmethod
+import pandas as pd 
+
+
+
+# abstract class for Data Ingesttion
+class DataIngestion(ABC):
+    @abstractmethod
+    def __init__(self):
+        pass 
+
+    @abstractmethod 
+    def ingest(self):
+        pass
+
+
+# class for ingesting Text Data from folders 
+class TextDataIngestion(DataIngestion):
+    def __init__(self, folder_name):
+        self.folder_name = folder_name
+    
+    def ingest(self):
+        """
+        Ingest data from a folder containing text files
+        
+        Returns: 
+            dict: A dictionary containing the text data
+        """
+        pdf_dict = {}
+    
+        # Walk through the directory
+        for root, dirs, files in os.walk(self.folder_name):
+            # Check if the current directory is not the root folder
+            if root != folder_name:
+                pdf_files = [file for file in files if file.lower().endswith('.pdf')]
+                if pdf_files:
+                    # Store the PDF files in the dictionary
+                    pdf_dict[os.path.basename(root)] = pdf_files
+
+        return pdf_dict
+
+
+# class for ingesting data from a csv
+class CSVDataIngestion(DataIngestion):
+    def __init__(self, folder_path):
+        self.folder_path = folder_path
+
+    def ingest(self):
+        """
+        Ingest data from a csv file 
+        """ 
+        csv_file_paths = []
+
+        # Iterate through the directory
+        for root, _, files in os.walk(self.folder_path):
+            for file in files:
+                if file.lower().endswith('.csv'):
+                    # Construct the full file path and add to the list
+                    full_path = os.path.join(root, file)
+                    csv_file_paths.append(full_path)
+
+        return csv_file_paths
+
+
+# class for ingesting data from api 
+
+
+# Factory Class for Data Ingestion 
+class DataIngestionFactory:
+    def __init__(self, strategy: DataIngestion):
+        """ 
+        Initialize the factory with a strategy
+        """
+        self.strategy = strategy
+
+    def set_strategy(self, strategy: DataIngestion):
+        """
+        Set the strategy for the factory
+        """
+        self.strategy = strategy
+    
+    def ingest_data(self):
+        """
+        Ingest data using the strategy
+        """
+        return self.strategy.ingest()
+
+
+    
